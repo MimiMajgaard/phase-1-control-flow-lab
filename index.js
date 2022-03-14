ce2c0a2cff2f7ec0ca3c7b88b3a192d04c4e55fe
Mimi Majgaard <mimimajgaard@gmail.com>
2022-03-13 18:39:15 -0700

    commit tree f4fbf749ca9e8b78f4d6cc8bc12c891f537ef501

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance <= 400) {
+  result = 'This one is on me!';
+  
+} else if (distance >= 2000) {
+  result = 'I will gladly take your thirty bucks.';
 
-function ternaryCheckCity(){
-  // Write your code here!
+} else if (distance > 2500) {
+  result = 'No can do.';
 }
+  return result 
+}
+scuberGreetingForFeet
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  let NYC = destination?  (result = 'Ok, sounds good.') : (result = 'No go.');
+return result
+}
+ternaryCheckCity
+
+function switchOnCharmFromTip(tipAmount) {
+ let tipAmount;
+ switch (switchOnCharmFromTip) {
+   case 'if the tip is generous':
+     tipAmount = 'Thank you so much.';
+    break;
+   case 'if the tip is not as generous':
+    tipAmount = 'Thank you.';
+    break;
+   case 'if anything else':
+     tipAmount = 'Bye.';
+ }
 }
